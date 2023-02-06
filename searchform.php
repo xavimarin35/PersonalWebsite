4ca28cfa3691080343e13ca405b4d32cb6dc9291
Xavi Marín Solà <xmarin98@gmail.com>
2023-02-07 00:30:09 +0100

    removing

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="search" action="<?php echo esc_url( home_url( '/' ) ); ?>/" method="get">
-	<input type="text" value="<?php esc_attr_e('Search and hit enter', 'harington') ?>" onblur="if(this.value == '') { this.value = '<?php esc_attr_e('Search and hit enter', 'harington') ?>'; }" onfocus="if(this.value == '<?php esc_attr_e('Search and hit enter', 'harington') ?>') { this.value = ''; }" size="30" id="search-field" name="s">
-	<label class="input_label"></label>
-</form>
